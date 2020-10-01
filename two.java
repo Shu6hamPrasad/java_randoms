3639cf8afb65278cc99c07091954c9b315888792
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2020-10-01 14:11:12 +0530

    Creates a random 4-Digit number....

---
+++ two.java
@@ -0,0 +1,13 @@
+package bxfx;
+import java.lang.Math;
+import java.util.Random; 
+public class two {
+	public static void main(String[] args) {
+		System.out.println("Random number generator");
+		int[] arr = {1,2,3,4,5,6,7,8,9,0};
+		System.out.print(+arr[new Random().nextInt(arr.length)]);
+		System.out.print(+arr[new Random().nextInt(arr.length)]);
+		System.out.print(+arr[new Random().nextInt(arr.length)]);
+		System.out.print(+arr[new Random().nextInt(arr.length)]);
+	}
+}
