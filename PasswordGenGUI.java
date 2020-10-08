766ed99655e4f30a95a5176b06ff5c71f3b40953
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2020-10-08 21:08:12 +0530

    Password Generator GUI
    This software requires PasswordGen2.java

--- PasswordGenGUI.java
+++ PasswordGenGUI.java
@@ -9,7 +9,7 @@ public class PasswordGenGUI implements ActionListener {
     private static JTextField userText;
     public static void main(String[] args){
         JFrame guilog = new JFrame("Credential Maker");
-        guilog.setSize(385, 165);
+        guilog.setSize(613, 165);
         guilog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         guilog.setResizable(false);
 
@@ -22,38 +22,52 @@ public class PasswordGenGUI implements ActionListener {
         guilog.setVisible(true);
 
     }
-	private static void placeComponents(JPanel panel) {
-		// TODO Auto-generated method stub
+    private static void placeComponents(JPanel panel) {
+        // TODO Auto-generated method stub
         Font  f3  = new Font(Font.DIALOG,  Font.PLAIN, 15);
         panel.setLayout(null);
-        
-        userLabel = new JLabel("How many digits you want?");
-        userLabel.setBounds(10, 10, 190, 25);
+
+        userLabel = new JLabel("How many digits you want? (Between 4 and 24)");
+        userLabel.setBounds(10, 10, 450, 25);
         userLabel.setFont(f3);
         panel.add(userLabel);
-        
+
         userText = new JTextField(20);
-        userText.setBounds(200, 10, 160, 25);
+        userText.setBounds(330, 10, 260, 25);
         userText.setFont(f3);
-        panel.add(userText); //I want to convert the input from this JTextField to an integer "int" variable.
-			     //Please :-}
-		             //I want to merge Java Swing GUI with my Random Password Generator to create a GUI Password Generator.
-		             //Link to Password Generator: "https://github.com/Shu6hamPrasad/java_randoms/blob/master/passwordGen2.java"
-        
-        
+        panel.add(userText);
+
+        userLabel = new JLabel("");
+        userLabel.setBounds(115, 50, 700, 25);
+        userLabel.setFont(f3);
+        //I want to convert the input from this JTextField to an integer "int" variable.
+        //Please :-}
+        //I want to merge Java Swing GUI with my Random Password Generator to create a GUI Password Generator.
+        //Link to Password Generator: "https://github.com/Shu6hamPrasad/java_randoms/blob/master/passwordGen2.java"
+
+
         loginButton = new JButton("GO!");
         loginButton.setBounds(10, 50, 95, 25);
         loginButton.setFont(f3);
         loginButton.addActionListener(new PasswordGenGUI());
         panel.add(loginButton);
-        
-        
-        
-		
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        panel.add(userLabel);
+
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        String digits = userText.getText();
+        String label;
+        try{
+            int digit = Integer.parseInt(digits);
+            PasswordGen2 passwordGen2 = new PasswordGen2();
+            String password = passwordGen2.password(digit);
+            label="Your pasword is: "+password;
+        }catch (Exception ex) {
+            label="Please enter only numeric digits.";
+        }
+        userLabel.setText(label);
+    }
 }
