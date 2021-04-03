f28fd34d0f49137c2668d033124c3e582c8cb3df
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-04-03 21:59:33 +0530

    Update roots.java

--- roots.java
+++ roots.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 public class roots {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //initialisation of scanner class
         double a,b,c;
         System.out.print("Enter a: ");
-        a = sc.nextInt();
+        a = sc.nextInt(); //inputting coefficient A
         System.out.print("Enter b: ");
-        b = sc.nextInt();
+        b = sc.nextInt(); //inputting coefficient B
         System.out.print("Enter c: ");
-        c = sc.nextInt();
-        double o = Math.pow(b,2);
-        double r = 4*(a*c);
-        double res = o-r;
-        System.out.println("Discriminant= "+res);
+        c = sc.nextInt(); //inputting coefficient C
+        double o = Math.pow(b,2); //calculating B raised to power of 2
+        double r = 4*(a*c); //calculating 4(AC)
+        double res = o-r; //using the discriminant formula: b^2-4(ac)
+        System.out.println("Discriminant= "+res); //printing the discriminant
         if (res > 0){
             System.out.println("Roots are unequal and real");
-            double checkpoint = Math.sqrt(res);
-            int checker = (int) checkpoint;
-            double tripleCheck = checker;
-            if(checkpoint == tripleCheck)
-                System.out.println("Roots are Rational");
+            //the following lines with "//." indicate that they are used for checking wheter a number is Rational/Irrational.
+            double checkpoint = Math.sqrt(res); //.
+            int checker = (int) checkpoint; //.
+            double tripleCheck = checker; //.
+            if(checkpoint == tripleCheck)  //.
+                System.out.println("Roots are Rational"); //.
             else
-                System.out.println("Irrational");
+                System.out.println("Irrational"); //.
         }
         else if (res == 0)
             System.out.println("Roots are equal.");
