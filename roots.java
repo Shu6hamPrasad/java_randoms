b4bf45443d71d0dbb7c2744992cdab681710c826
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-04-03 21:53:28 +0530

    roots.java
    This program takes the coefficients of a Quadratic Equation and then checks whether the roots are equal or real or imaginary. if the roots are real then it will check that if the roots are rational or not.

---
+++ roots.java
@@ -0,0 +1,32 @@
+package Package1;
+import java.util.Scanner;
+public class roots {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a,b,c;
+        System.out.print("Enter a: ");
+        a = sc.nextInt();
+        System.out.print("Enter b: ");
+        b = sc.nextInt();
+        System.out.print("Enter c: ");
+        c = sc.nextInt();
+        double o = Math.pow(b,2);
+        double r = 4*(a*c);
+        double res = o-r;
+        System.out.println("Discriminant= "+res);
+        if (res > 0){
+            System.out.println("Roots are unequal and real");
+            double checkpoint = Math.sqrt(res);
+            int checker = (int) checkpoint;
+            double tripleCheck = checker;
+            if(checkpoint == tripleCheck)
+                System.out.println("Roots are Rational");
+            else
+                System.out.println("Irrational");
+        }
+        else if (res == 0)
+            System.out.println("Roots are equal.");
+        else
+            System.out.println("Roots are imaginary.");
+    }
+}
