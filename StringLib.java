f17f89e269fa4465e4967c9660afe9356fad88c2
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-08-25 14:29:28 +0530

    Update StringLib.java

--- StringLib.java
+++ StringLib.java
@@ -1,7 +1,7 @@
 /*
     Copyright © Shubham Prasad. All Rights Reserved.
     Licensed under the GNU GPL license.
-    https://github.com/shu6hamprasad/
+    https://github.com/Shu6hamPrasad/
     IDE Used: IntelliJ IDEA Ultimate 2021.2
 */
 
@@ -16,36 +16,43 @@ class StringLib {
 
     public static char[] toCharArray(String s) {
         char[] f = new char[s.length()];
+        if (!isEmpty(s)) {
+            for (int i = 0; i < s.length(); ++i) {
+                f[i] = s.charAt(i);
+            }
 
-        for(int i = 0; i < s.length(); ++i) {
-            f[i] = s.charAt(i);
         }
-
         return f;
     }
 
     public static String toUpperCase(String x) {
         char[] xArr = x.toCharArray();
-
-        for(int i = 0; i < xArr.length; ++i) {
-            if (xArr[i] >= 'a' && xArr[i] <= 'z') {
-                xArr[i] = (char)(xArr[i] - 32);
+        if (isEmpty(x)) {
+            return x;
+        } else {
+            for(int i = 0; i < xArr.length; ++i) {
+                if (xArr[i] >= 'a' && xArr[i] <= 'z') {
+                    xArr[i] = (char)(xArr[i] - 32);
+                }
             }
-        }
 
-        return String.valueOf(xArr);
+            return String.valueOf(xArr);
+        }
     }
 
     public static String toLowerCase(String x) {
         char[] xArr = x.toCharArray();
-
-        for(int i = 0; i < xArr.length; ++i) {
-            if (xArr[i] >= 'A' && xArr[i] <= 'Z') {
-                xArr[i] = (char)(xArr[i] + 32);
+        if (isEmpty(x)) {
+            return x;
+        } else {
+            for(int i = 0; i < xArr.length; ++i) {
+                if (xArr[i] >= 'A' && xArr[i] <= 'Z') {
+                    xArr[i] = (char)(xArr[i] + 32);
+                }
             }
-        }
 
-        return String.valueOf(xArr);
+            return String.valueOf(xArr);
+        }
     }
 
     public static int indexOf(String s, char c) {
@@ -62,35 +69,40 @@ class StringLib {
     }
 
     public static int indexOf(String str, String substr) {
-        int strlen = str.length();
-        int substrlen = substr.length();
-        int count = 0;
-        int substrlen2 = substr.length();
-        if (substrlen >= 1) {
-            for(int i = 0; i < strlen - substrlen + 1; ++i) {
-                String A = subString(str, i, substrlen2);
-                if (equalsIgnoreCase(A, substr)) {
-                    return count + 1;
-                }
+        if (!isEmpty(str)) {
+            int strlen = str.length();
+            int substrlen = substr.length();
+            int count = 0;
+            int substrlen2 = substr.length();
+            if (substrlen >= 1) {
+                for (int i = 0; i < strlen - substrlen + 1; ++i) {
+                    String A = subString(str, i, substrlen2);
+                    if (equalsIgnoreCase(A, substr)) {
+                        return count + 1;
+                    }
 
-                ++count;
-                ++substrlen2;
+                    ++count;
+                    ++substrlen2;
+                }
             }
 
         }
         return -1;
-    } //Not my function. Taken from StackOverflow.
+    }   // Not my code. Taken from StackOverflow.
 
     public static int lastIndexOf(String s, char c) {
         int out = -1;
-
-        for(int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == c) {
-                out = i;
+        if (isEmpty(s)) {
+            return -1;
+        } else {
+            for(int i = 0; i < s.length(); ++i) {
+                if (s.charAt(i) == c) {
+                    out = i;
+                }
             }
-        }
 
-        return out;
+            return out;
+        }
     }
 
     public static String concat(String x, String y) {
@@ -193,7 +205,7 @@ class StringLib {
     public static String replace(String s, char x, char y) {
         char[] c = toCharArray(s);
 
-        for (int i = 0; i < c.length; i++) {
+        for(int i = 0; i < c.length; ++i) {
             if (c[i] == x) {
                 c[i] = y;
             }
@@ -201,5 +213,80 @@ class StringLib {
 
         return String.valueOf(c);
     }
-    
+
+    public static char[] getChars(String s, int x, int y) {
+        char[] out = new char[y - x];
+
+        for(int i = x; i < y; ++i) {
+            out[i] = s.charAt(i);
+        }
+
+        return out;
+    }
+
+    public static char[] getChars(String x, int y) {
+        return getChars(x, y, x.length());
+    }
+
+    public static boolean isEmpty(String s) {
+        return s.length() == 0;
+    }
+
+    public static String trim(String s) {
+        int fs = 0;
+        int ff = 0;
+
+        int i;
+        for(i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) != ' ') {
+                fs = i;
+                break;
+            }
+        }
+
+        for(i = s.length() - 1; i >= 0; --i) {
+            if (s.charAt(i) != ' ') {
+                ff = i + 1;
+                break;
+            }
+        }
+
+        return subString(s, fs, ff);
+    }
+
+    public static boolean isUpperCase(String s) {
+        int count = 0;
+        boolean out = false;
+        if (isEmpty(s)) {
+            return false;
+        } else {
+            for(int i = 0; i < s.length() && charAt(s, i) >= 'A' && charAt(s, i) <= 'Z'; ++i) {
+                ++count;
+            }
+
+            if (count == s.length() - 1) {
+                out = true;
+            }
+
+            return out;
+        }
+    }
+
+    public static boolean isLowerCase(String s) {
+        int count = 0;
+        boolean out = false;
+        if (isEmpty(s)) {
+            return false;
+        } else {
+            for(int i = 0; i < s.length() && charAt(s, i) >= 'a' && charAt(s, i) <= 'z'; ++i) {
+                ++count;
+            }
+
+            if (count == s.length() - 1) {
+                out = true;
+            }
+
+            return out;
+        }
+    }
 }
